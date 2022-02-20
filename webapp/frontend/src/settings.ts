4d711d1406873ec08b4b5beb12fcfb8fc31d85fe
Alexander Baltser <aubalcer@gmail.com>
2022-02-20 14:47:59 +0200

    Revert settings

--- webapp/frontend/src/settings.ts
+++ webapp/frontend/src/settings.ts
@@ -4,6 +4,6 @@ const URL_MAP: Record<string, string> = {
     'development': 'http://localhost:8080/api'
 };
 export const settings = {
-    //apiUrl: URL_MAP[process.env.NODE_ENV] ?? 'http://localhost:8080/api'
-    apiUrl: '/api'
+    apiUrl: URL_MAP[process.env.NODE_ENV] ?? 'http://localhost:8080/api'
+   // apiUrl: '/api'
 }
